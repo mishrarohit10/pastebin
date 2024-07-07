35568f0af09bf8e8d6050fbfbe253c0d270199fc
rohit <mishrarohit316x@gmail.com>
2024-07-07 17:11:52 +0530

    completed ui

--- pastebin/src/NavBar.tsx
+++ pastebin/src/NavBar.tsx
@@ -3,12 +3,17 @@ export function NavBar() {
     return (
         <div className="navbar-container">
             <nav className="navbar">
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#news">News</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#about">About</a></li>
-                </ul>
+                <div className="left">
+                    <h1>Pastebin</h1>
+                </div>
+                <div className="right">
+                    <ul>
+                        {/* <li><a href="#home">Login</a></li> */}
+                        {/* <li><a href="#news">News</a></li> */}
+                        {/* <li><a href="#contact">Contact</a></li> */}
+                        {/* <li><a href="#about">About</a></li> */}
+                    </ul>
+                </div>
             </nav>
         </div>
     );
